bf527af085c2bb89938f092f851442c6b4f53e4c
mrullldhm <mrullldhm94@gmail.com>
2025-03-13 05:30:03 +0800

    lesson 6

--- lesson-6-functions/src/index.ts
+++ lesson-6-functions/src/index.ts
@@ -1,12 +1,7 @@
-// enums are a way to define a set of named constants
+function calculateTax(income: number, taxYear = 2022): number {
+    if ( taxYear < 2022) 
+        return income * 1.2
+    return income * 1.3
+}
 
-const small = 1;
-const medium = 2;
-const large = 3;
-
-// enums example
-// default value of enums start with 0, 1, 2, 3, ,,,
-// can set the value of enums manually
-const enum Size {Small = 1, Medium = 2, Large = 3}
-let mySize: Size = Size.Medium
-console.log(mySize)
+calculateTax(10_000)
