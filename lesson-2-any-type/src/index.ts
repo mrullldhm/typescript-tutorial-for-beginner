f03ce21e87c96b4b541b2938967e6318f59697cd
mrullldhm <mrullldhm94@gmail.com>
2025-03-13 03:43:12 +0800

    lesson 2

---
+++ lesson-2-any-type/src/index.ts
@@ -0,0 +1,15 @@
+let sales = 123_456_789;
+let course = 'TypeScript';
+let is_published = true;
+
+// any type
+let level
+level = 1;
+level = 'a';
+
+// Not recommended, change noImplicitAny to false
+function render (document) {
+    console.log(document);
+}
+
+
